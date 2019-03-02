3a47e2edec1ed5c9b248152d2f5e2242fcd2f13a
Dave Longley <dlongley@digitalbazaar.com>
2019-03-02 15:51:24 -0500

    Move route handlers to accommodate pretender limitations.

--- MockStorage.js
+++ MockStorage.js
@@ -9,147 +9,75 @@ export class MockStorage {
   constructor({server}) {
     this.dataHubs = new Map();
     this.documents = new Map();
-    this.indexes = new Map();
 
     const root = '/data-hubs';
-    const routes = {
+    const routes = this.routes = {
       dataHubs: root,
-      dataHub: new RegExp(`${root}/:dataHubId`),
-      documents: new RegExp(`${root}/:dataHubId/documents`),
-      document: new RegExp(`${root}/:dataHubId/documents/:docId`),
-      dataHub: new RegExp(`${root}/:dataHubId/query`)
+      dataHub: `${root}/:dataHubId`,
+      documents: `${root}/:dataHubId/documents`,
+      dataHub: `${root}/:dataHubId/query`
     };
 
-    server.map(() => {
-      // create a new data hub
-      server.post(routes.dataHubs, request => {
-        const config = JSON.parse(request.requestBody);
-        // TODO: validate `config`
-        config.id = uuid();
-        this.dataHubs.set(config.id, {
-          config,
-          documents: new Map(),
-          indexes: {
-            equals: new Map(),
-            has: new Map()
-          }
-        });
-        const location = `http://localhost:9876/${root}/${config.id}`;
-        return [200, {location, json: true}, config];
-      });
+    // create a new data hub
+    server.post(routes.dataHubs, request => {
+      const config = JSON.parse(request.requestBody);
+      // TODO: validate `config`
+      config.id = uuid();
+      const dataHub = {
+        config,
+        documents: new Map(),
+        indexes: new Map()
+      };
+      this.dataHubs.set(config.id, dataHub);
+      this.mapDocumentHandlers({server, dataHub});
+      const location = `http://localhost:9876/${root}/${config.id}`;
+      return [200, {location, json: true}, config];
+    });
 
-      // insert a document into a data hub
-      server.post(routes.documents, request => {
-        const {dataHubId} = request.params;
-        const dataHub = this.dataHubs.get(dataHubId);
-        if(!dataHub) {
-          // data hub does not exist
-          return [404];
-        }
+    // insert a document into a data hub
+    server.post(routes.documents, request => {
+      const {dataHubId} = request.params;
+      const dataHub = this.dataHubs.get(dataHubId);
+      if(!dataHub) {
+        // data hub does not exist
+        return [404];
+      }
 
-        const doc = JSON.parse(request.requestBody);
-        if(this.dataHub.has(doc.id)) {
-          return [409];
-        }
-        this.store(dataHub, doc);
-        const location =
-          `http://localhost:9876/${root}/${dataHubId}/documents/${doc.id}`;
-        return [201, {location}];
-      });
+      const doc = JSON.parse(request.requestBody);
+      if(dataHub.documents.has(doc.id)) {
+        return [409];
+      }
+      this.store({dataHub, doc});
+      const location =
+        `http://localhost:9876/${root}/${dataHubId}/documents/${doc.id}`;
+      return [201, {location}];
+    });
 
-      server.post(routes.document, request => {
-        const {dataHubId, docId} = request.params;
-        const dataHub = this.dataHubs.get(dataHubId);
-        if(!dataHub) {
-          // data hub does not exist
-          return [404];
-        }
+    // query a data hub
+    server.post(routes.query, request => {
+      const {dataHubId} = request.params;
+      const dataHub = this.dataHubs.get(dataHubId);
+      if(!dataHub) {
+        // data hub does not exist
+        return [404];
+      }
 
-        const doc = JSON.parse(request.requestBody);
-        if(docId !== doc.id) {
-          return [400];
-        }
-        this.store(dataHub, doc);
-        return [200];
-      });
+      const query = JSON.parse(request.requestBody);
+      const index = dataHub.indexes[query.index];
+      if(!index) {
+        // index does not exist
+        return [404];
+      }
 
-      server.get(routes.document, request => {
-        const {dataHubId, docId} = request.params;
-        const dataHub = this.dataHubs.get(dataHubId);
-        if(!dataHub) {
-          // data hub does not exist
-          return [404];
-        }
-
-        const doc = dataHub.documents.get(docId);
-        if(!doc) {
-          return [404];
-        }
-        return [200, {json: true}, doc];
-      });
-
-      server.delete(routes.document, request => {
-        const {dataHubId, docId} = request.params;
-        const dataHub = this.dataHubs.get(dataHubId);
-        if(!dataHub) {
-          // data hub does not exist
-          return [404];
-        }
-
-        if(!dataHub.documents.has(docId)) {
-          return [404];
-        }
-        dataHub.documents.delete(docId);
-        return [204];
-      });
-
-      server.post(routes.query, request => {
-        const {dataHubId} = request.params;
-        const dataHub = this.dataHubs.get(dataHubId);
-        if(!dataHub) {
-          // data hub does not exist
-          return [404];
-        }
-
-        const query = JSON.parse(request.requestBody);
-        const index = dataHub.indexes[query.index];
-        if(!index) {
-          // index does not exist
-          return [404];
-        }
-
-        // build results
-        const results = [];
-        if(query.equals) {
-          for(const equals of query.equals) {
-            let matches = null;
-            for(const key in equals) {
-              const value = equals[key];
-              const docs = this.find(
-                {index: dataHub.indexes.equals, key: key + '=' + value});
-              if(!matches) {
-                // first result
-                matches = docs;
-              } else {
-                // remove any docs from `matches` that are not in `docs`
-                matches = matches.filter(x => docs.includes(x));
-                if(matches.length === 0) {
-                  break;
-                }
-              }
-            }
-            (matches || []).forEach(x => {
-              if(!results.includes(x)) {
-                results.push(x);
-              }
-            });
-          }
-        }
-
-        if(query.has) {
+      // build results
+      const results = [];
+      if(query.equals) {
+        for(const equals of query.equals) {
           let matches = null;
-          for(const key of query.has) {
-            const docs = dataHub.find({index: this.indexes.has, key});
+          for(const key in equals) {
+            const value = equals[key];
+            const docs = this.find(
+              {index: index.equals, key: key + '=' + value});
             if(!matches) {
               // first result
               matches = docs;
@@ -161,15 +89,37 @@ export class MockStorage {
               }
             }
           }
-          results.push(...(matches || []));
+          (matches || []).forEach(x => {
+            if(!results.includes(x)) {
+              results.push(x);
+            }
+          });
         }
+      }
 
-        return [200, {json: true}, results];
-      });
+      if(query.has) {
+        let matches = null;
+        for(const key of query.has) {
+          const docs = this.find({index: index.has, key});
+          if(!matches) {
+            // first result
+            matches = docs;
+          } else {
+            // remove any docs from `matches` that are not in `docs`
+            matches = matches.filter(x => docs.includes(x));
+            if(matches.length === 0) {
+              break;
+            }
+          }
+        }
+        results.push(...(matches || []));
+      }
+
+      return [200, {json: true}, results];
     });
   }
 
-  store(dataHub, doc) {
+  store({dataHub, doc}) {
     dataHub.documents.set(doc.id, doc);
     for(const entry of doc.indexed) {
       let index = dataHub.indexes[entry.hmac.id];
@@ -210,5 +160,41 @@ export class MockStorage {
       return [];
     }
     return [...docSet];
+  }
+
+  mapDocumentHandlers({server, dataHub}) {
+    const root = `${this.routes.dataHubs}/${dataHub.config.id}/documents`;
+    const route = `${root}/:docId`;
+
+    // update a document
+    server.post(route, request => {
+      const {docId} = request.params;
+      const doc = JSON.parse(request.requestBody);
+      if(docId !== doc.id) {
+        return [400];
+      }
+      this.store({dataHub, doc});
+      return [200];
+    });
+
+    // get a document
+    server.get(route, request => {
+      const {docId} = request.params;
+      const doc = dataHub.documents.get(docId);
+      if(!doc) {
+        return [404];
+      }
+      return [200, {json: true}, doc];
+    });
+
+    // delete a document
+    server.delete(route, request => {
+      const {docId} = request.params;
+      if(!dataHub.documents.has(docId)) {
+        return [404];
+      }
+      dataHub.documents.delete(docId);
+      return [204];
+    });
   }
 }
